664cc2c5a2abfa8b1de11508360a717765e48730
Donchelox <inside.4.me@gmail.com>
2014-08-04 20:58:23 -0400

    Clase1

---
+++ Clase 1/codigo.js
@@ -0,0 +1,33 @@
+
+
+
+
+var peso;
+var pesoEnMarte; //CamelCase
+alert("¿Quieres saber cuál es tu peso en Marte?")
+peso = prompt("Cual es tu peso en Kg?", "70")
+peso = parseInt(peso);
+
+pesoEnMarte = (peso/ 9.81) 3.711;
+
+alert("Tu peso en marte es:"pesoEnMarte" + " kg)	
+
+alert(peso + " kg")
+
+
+
+
+
+
+
+
+
+
+	//Alert:función
+	//() : Parametros de la función
+	//"" : Cadenas de texto
+	//var nombre = "Marcelo";
+	//var apellido = "Vasquez";
+	//var edad = "25";
+
+//alert(nombre + " " + apellido + "\n " +edad + " años")
